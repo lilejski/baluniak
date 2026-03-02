7359a4e48b7139defb6643c08802b4d72cc0e1c2
lilejski <l.baluniak@gmail.com>
2026-03-02 22:11:16 +0100

    feat: implement new favicons in layout metadata

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -46,6 +46,26 @@ export async function generateMetadata({
       type: "website",
     },
     robots: { index: true, follow: true },
+    icons: {
+      icon: [
+        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        { url: "/favicon.ico", sizes: "any" },
+      ],
+      apple: [
+        { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      ],
+      other: [
+        {
+          rel: "android-chrome-192x192",
+          url: "/android-chrome-192x192.png",
+        },
+        {
+          rel: "android-chrome-512x512",
+          url: "/android-chrome-512x512.png",
+        },
+      ],
+    },
   };
 }
 
