39197981264b6f51943071733d5221e822221512
lilejski <l.baluniak@gmail.com>
2026-04-02 21:17:28 +0200

    chore: remove KreatorCTA from home page per user request

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -8,7 +8,6 @@ import { Projects } from "@/components/sections/Projects";
 import { TechStackTrust } from "@/components/sections/TechStackTrust";
 import { ModularStack } from "@/components/sections/ModularStack";
 import { ContactSection } from "@/components/sections/ContactSection";
-import { KreatorCTA } from "@/components/sections/KreatorCTA";
 
 export default function Home() {
   return (
@@ -24,7 +23,6 @@ export default function Home() {
         <TryLiveIntro />
         <AIDuelLayout />
       </section>
-      <KreatorCTA />
       <ModularStack />
       <Projects />
       <WhyProductEngineer />
